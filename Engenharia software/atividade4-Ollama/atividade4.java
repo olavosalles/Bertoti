c354a714b9bcce3317d13bcd15c39858bfc99c14
olavosalles <olavosalles123@gmail.com>
2026-08-17 19:52:18 -0300

    Implement chat functionality using Ollama API

---
+++ Engenharia software/atividade4-Ollama/atividade4.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import io.github.ollama4j.Ollama;
+import io.github.ollama4j.models.chat.OllamaChatMessageRole;
+import io.github.ollama4j.models.chat.OllamaChatRequest;
+import io.github.ollama4j.models.chat.OllamaChatStreamObserver;
+import io.github.ollama4j.models.generate.OllamaGenerateTokenHandler;
+
+
+public class atividade4 {
+
+    public static void main(String[] args) throws Exception {
+
+
+
+        Ollama ollama = new Ollama("http://127.0.0.1:11434/");
+        String model = "gemma3:270m";
+        ollama.pullModel(model);
+        OllamaChatRequest builder = OllamaChatRequest.builder().withModel(model);
+
+        OllamaChatRequest chatRequest =
+                builder.withMessage(
+                                OllamaChatMessageRole.USER,
+                                "Give me a summary of the book 'The Great Gatsby'")
+                        .build();
+
+        // Define a stream observer.
+        OllamaChatStreamObserver streamObserver = new OllamaChatStreamObserver();
+
+        // If thinking tokens are found, print them in lowercase :)
+        streamObserver.setThinkingStreamHandler(
+                new OllamaGenerateTokenHandler() {
+                    @Override
+                    public void accept(String message) {
+                        System.out.print(message.toUpperCase());
+                    }
+                });
+        // Response tokens to be printed in lowercase
+        streamObserver.setResponseStreamHandler(
+                new OllamaGenerateTokenHandler() {
+                    @Override
+                    public void accept(String message) {
+                        System.out.print(message.toLowerCase());
+                    }
+                });
+
+        ollama.chat(chatRequest, streamObserver);
+    }
+}
